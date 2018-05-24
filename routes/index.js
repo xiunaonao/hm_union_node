e19913f57a1a5b940475159d8f9a0a23cc3ed216
xiunaonao <onlymyalways@163.com>
2018-05-24 17:26:45 +0800

    工会身份验证

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,62 @@
 var express = require('express');
+let crypto = require('crypto');
+let http=require('http');
 var router = express.Router();
+
+let aesMethod=(token,callback)=>{
+  let key='UNIONFROMZJBYHMS'
+  let iv='1234567812345678'
+  token=token.replace(' ','+')
+  var crypted = new Buffer(token, 'base64').toString('binary')
+
+  var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv)
+  var decoded = decipher.update(crypted, 'binary', 'utf8')
+  decoded += decipher.final('utf8')
+  callback(decoded)
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.sendFile('index.html');
 });
+
+
+router.get('/valid',function(req,res,next){
+	var token=req.query.token
+	aesMethod(token,(decoded)=>{
+		let data=JSON.parse(decoded)
+
+		let opt={
+			host:'cj.123zou.com',
+			path:'/MobileNews/get_member_info?member_id='+data.memberId,
+			method: 'GET'
+		}
+		console.log(opt.host+opt.path)
+		let _req=http.request(opt,(_res)=>{
+			let data='';
+		    //console.log('headers:'+JSON.stringify(_res.headers));  
+		    _res.setEncoding('utf-8');  
+		    _res.on('data',function(datas){  
+		        data+=datas;
+		    });  
+		    _res.on('end',function(){
+		    	data=JSON.parse(data);
+		    	console.log(data);
+		    	res.render('index',{url:req.query.return,token:req.query.token,data:data})
+		        //console.log('No more data in response.');  
+		    });  
+
+		})
+		_req.on('error',function(err){  
+		    console.error(err)
+		})
+		_req.write('');
+		_req.end();
+		//req.write(str)
+	})
+	
+})
 
 
 
--- routes/system.js
+++ routes/system.js
@@ -2,22 +2,29 @@ var express = require('express');
 let crypto = require('crypto');
 var router = express.Router();
 
+let aesMethod=(token,callback)=>{
+  token=token.replace(' ','+')
+  let key='UNIONFROMZJBYHMS'
+  let iv='1234567812345678'
+  var crypted = new Buffer(token, 'base64').toString('binary')
+
+  var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv)
+  console.log(decipher)
+  var decoded = decipher.update(crypted, 'binary', 'utf8')
+  decoded += decipher.final('utf8')
+  callback(decoded)
+}
+
 /* GET users listing. */
 router.get('/getUser', (req, res, next)=>{
-  let json={};
-  let token=req.query.token;
-
-  let key='UNIONFROMZJBYHMS';
-  let iv='1234567812345678';
-
-  var crypted = new Buffer(token, 'base64').toString('binary');
-
-  var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-  console.log(decipher)
-  var decoded = decipher.update(crypted, 'binary', 'utf8');
-  decoded += decipher.final('utf8');
-  console.log(decoded);
-  res.json({success:0,str:decoded,data:{}});
+  let token=req.query.token.replace(' ','+')
+  let user=JSON.parse(decoded)
+  aesMethod(token,(decoded)=>{
+    res.json({success:0,str:decoded,data:{}})
+  })
+  
 });
+
+
 
 module.exports = router;
